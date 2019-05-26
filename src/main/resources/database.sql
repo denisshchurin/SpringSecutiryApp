f553ae15d3d15d3eab383bf65555479ebb8cc659
denisshchurin <denisshchurin@gmail.com>
2019-05-26 11:54:57 +0200

    create database MySql spring_secutity_app and add file database.sql

---
+++ src/main/resources/database.sql
@@ -0,0 +1,37 @@
+-- Table: users
+CREATE TABLE users
+(
+    id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+)
+    ENGINE = InnoDB;
+
+-- Table: roles
+CREATE TABLE roles
+(
+    id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+)
+    ENGINE = InnoDB;
+
+-- Table for mapping user and roles: user_roles
+CREATE TABLE users_roles(
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+
+    UNIQUE (user_id, role_id)
+)
+    ENGINE = InnoDB;
+
+-- Insert data
+
+INSERT INTO users VALUES (1, 'denis', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG');
+
+INSERT INTO roles VALUES (1, 'ROLE-USER');
+INSERT INTO roles VALUES (2, 'ROLE-ADMIN');
+
+INSERT INTO users_roles VALUES (1, 2);
